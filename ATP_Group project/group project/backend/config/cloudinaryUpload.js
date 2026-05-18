812ad41885541235bf13ecfc8b6ba366d13b860b
ananyaa241 <brightside7806@gmail.com>
2026-05-18 10:26:05 +0530

    Delete ATP_Group project/group project directory

--- ATP_Group project/group project/backend/config/cloudinaryUpload.js
+++
@@ -1,9 +0,0 @@
-import cloudinary from "./cloudinary.js";
-
-export const cloudinaryUpload = async (filePath) => {
-
-    return await cloudinary.uploader.upload(filePath, {
-        folder: "hospital-management"
-    });
-
-};
